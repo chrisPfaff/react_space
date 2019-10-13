71acfe32febc73f00ddf5b2c7f2f33b1915cd498
Chris Pfaff <chrispfaff10@gmail.com>
2019-10-13 10:56:52 -0500

    added hook for image fetch

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,15 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const Home = () => {
-  const [image, getImage] = useState();
+function Home(props) {
+  const [image, getImage] = useState(null);
+
+  console.log(image);
+
+  const url =
+    "https://api.nasa.gov/planetary/apod?api_key=raP0qIkThJJqXBjcPypF4LNUhEnHUndMyyeYczup";
+
+  const fetchData = data => {
+    getImage(data);
+  };
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then(function(response) {
+        fetchData(response.data.url);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  });
 
   return (
     <div className="Home">
       <div className="Home_body">
-        <h1>hello</h1>
+        <img src={image} alt="nasa space" />
       </div>
     </div>
   );
-};
+}
 
 export default Home;
