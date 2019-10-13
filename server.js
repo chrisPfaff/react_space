028d160d3cc49b9840dd64afade5d1d57bca691b
Chris Pfaff <chrispfaff10@gmail.com>
2019-10-13 18:05:10 -0500

    added fetch to grab data from api

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+var cors = require("cors");
+
 const app = express();
+app.use(cors());
+
+const axios = require("axios");
 const port = 8080;
+
+require("dotenv").config();
 
 app.use(express.static(path.join(__dirname, "./client/build")));
 
@@ -10,8 +17,14 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build", "index.html"));
 });
 
-app.get("/working", (req, res) => {
-  res.send("workings");
+app.get("/getimage", (req, res) => {
+  const apiKey = process.env.API_KEY;
+
+  axios
+    .get(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`)
+    .then(response => {
+      res.send(response.data);
+    });
 });
 
 app.listen(process.env.PORT || port, () => {
