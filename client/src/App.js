65a01ce6efaaa10107f0c1921bddd9bd11ebf777
Chris Pfaff <chrispfaff10@gmail.com>
2019-10-14 13:24:31 -0500

    implemented router

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,32 @@
 import React from "react";
 import Home from "./components/Home.js";
+import Blah from "./components/Blah.js";
 import "./styles/App.scss";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App_header">
-        <img className="App_logo" src="logo.png" alt="main header image" />
-      </header>
-      <Home data={"data"} />
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App_header">
+          <img className="App_logo" src="logo.png" alt="main header image" />
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/blah">Blah</Link>
+          </li>
+        </header>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/blah">
+            <Blah />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
