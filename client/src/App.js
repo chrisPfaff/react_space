697d10c6b513aaedd861e2f35bfa24c30298430b
Chris Pfaff <chrispfaff10@gmail.com>
2019-10-14 20:39:58 -0500

    added epic component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Home from "./components/Home.js";
-import Blah from "./components/Blah.js";
+import Epic from "./components/Epic.js";
 import "./styles/App.scss";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
@@ -14,15 +14,15 @@ function App() {
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/blah">Blah</Link>
+            <Link to="/epic">E.P.I.C</Link>
           </li>
         </header>
         <Switch>
           <Route exact path="/">
             <Home />
           </Route>
-          <Route exact path="/blah">
-            <Blah />
+          <Route exact path="/epic">
+            <Epic />
           </Route>
         </Switch>
       </div>
