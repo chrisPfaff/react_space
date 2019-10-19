5bb2f9957b751523a2cd8ea3b27bd45865b5cd51
Chris Pfaff <chrispfaff10@gmail.com>
2019-10-19 14:29:05 -0500

    ading loading component

---
+++ client/src/components/Loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="loadingContainer">
+      <img className="notFound_image" src="404.png" alt="loading" />
+    </div>
+  );
+};
+
+export default Loading;
