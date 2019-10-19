2c61c82cd88db2000cfc9c9610599b1228fbf5de
Chris Pfaff <chrispfaff10@gmail.com>
2019-10-18 23:51:32 -0500

    adding dark mode

--- client/src/components/Darkmode.js
+++ client/src/components/Darkmode.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Darkmode.scss";
 
 const Darkmode = props => {
+  const [dark, setDark] = useState(false);
+
+  const clickFunction = e => {
+    let body = document.querySelector("body");
+    console.log(body);
+    if (dark === false) {
+      setDark(true);
+      body.classList.add("dark");
+    } else {
+      setDark(false);
+      body.classList.remove("dark");
+    }
+  };
+
   return (
     <div className="DarkmodeContainer">
       <span className="DarkmodeContainer_name">Darkmode</span>
       <label className="DarkmodeContainer_switch">
-        <input type="checkbox" />
+        <input onClick={clickFunction} type="checkbox" />
         <span className="slider round" />
       </label>
     </div>
