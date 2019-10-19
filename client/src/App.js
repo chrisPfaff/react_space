93b822e19ca635fc50a9726bf84bce60534260f6
Chris Pfaff <chrispfaff10@gmail.com>
2019-10-19 14:28:14 -0500

    added z index for logo

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/Home.js";
 import Epic from "./components/Epic.js";
 import Notfound from "./components/Notfound.js";
 import Darkmode from "./components/Darkmode.js";
+import Loading from "./components/Loading.js";
 
 import "./styles/App.scss";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
@@ -12,7 +13,16 @@ function App() {
     <Router>
       <div className="App">
         <header className="App_header">
-          <img className="App_logo" src="logo.png" alt="main header image" />
+          <img
+            className="App_logo_light"
+            src="logo.png"
+            alt="main header image"
+          />
+          <img
+            className="App_logo_dark"
+            src="logoDark.png"
+            alt="main header image"
+          />
           <div className="App_nav">
             <li>
               <Link to="/">Home</Link>
