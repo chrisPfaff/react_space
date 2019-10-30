b2e311d4298eedbccba5526c3fbe7adedc5b6c02
Chris Pfaff <chrispfaff10@gmail.com>
2019-10-29 20:10:13 -0500

    added axios fetch to server

--- client/src/components/Eonet.js
+++ client/src/components/Eonet.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function Eonet(props) {
-  return <div>hello eonet</div>;
+  const [eonetData, setData] = useState([]);
+
+  useEffect(() => {
+    const getEonetData = async () => {
+      await axios
+        .get("http://localhost:8080/eonet")
+        .then(function(response) {
+          console.log(response.data.events[0].sources[0].url);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    };
+    getEonetData();
+  });
+
+  console.log(eonetData);
+  return (
+    <div>
+      <h1>Eonet</h1>
+      <h2>Earth Observatory Natural Event Tracker (EONET)</h2>
+    </div>
+  );
 }
 
 export default Eonet;
