0abc200c64fdfe7e9092617b78fbb2d02edc7731
Chris Pfaff <chrispfaff10@gmail.com>
2019-10-29 20:15:41 -0500

    added iframe to show webpage data

--- client/src/components/Eonet.js
+++ client/src/components/Eonet.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+import "../styles/Eonet.scss";
+
 function Eonet(props) {
   const [eonetData, setData] = useState([]);
 
@@ -9,7 +11,7 @@ function Eonet(props) {
       await axios
         .get("http://localhost:8080/eonet")
         .then(function(response) {
-          console.log(response.data.events[0].sources[0].url);
+          setData(response.data.events[0].sources[0].url);
         })
         .catch(function(error) {
           console.log(error);
@@ -20,9 +22,12 @@ function Eonet(props) {
 
   console.log(eonetData);
   return (
-    <div>
-      <h1>Eonet</h1>
-      <h2>Earth Observatory Natural Event Tracker (EONET)</h2>
+    <div className="eonet">
+      <h1 className="eonet_title">Eonet</h1>
+      <h2 className="eonet_title">
+        Earth Observatory Natural Event Tracker (EONET)
+      </h2>
+      <iframe className="eonet_iframe" src={eonetData} frameborder="0"></iframe>
     </div>
   );
 }
