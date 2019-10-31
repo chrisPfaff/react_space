4fbb5ffa8fd7d999030c5b0e14bd651b86ddb0e0
Chris Pfaff <chrispfaff10@gmail.com>
2019-10-30 21:34:58 -0500

    added select box for eonet fetch

--- client/src/components/Eonet.js
+++ client/src/components/Eonet.js
@@ -11,23 +11,38 @@ function Eonet(props) {
       await axios
         .get("http://localhost:8080/eonet")
         .then(function(response) {
-          setData(response.data.events[0].sources[0].url);
+          setData(response.data.events);
         })
         .catch(function(error) {
           console.log(error);
         });
     };
     getEonetData();
-  });
+  }, []);
 
   console.log(eonetData);
   return (
     <div className="eonet">
-      <h1 className="eonet_title">Eonet</h1>
-      <h2 className="eonet_title">
+      <h1 className="eonet_title hover">Eonet</h1>
+      <h2 className="eonet_title hover">
         Earth Observatory Natural Event Tracker (EONET)
       </h2>
-      <iframe className="eonet_iframe" src={eonetData} frameborder="0"></iframe>
+      <div className="eonet_body">
+        <select name="eonetSelect" id="eonetSelect" className="eonet_select">
+          <option value="6">Drought</option>
+          <option value="7">Dust and Haze</option>
+          <option value="16">Earthquakes</option>
+          <option value="9">Floods</option>
+          <option value="14">Landslides</option>
+          <option value="19">Manmade</option>
+          <option value="15">Sea and Lake Ice</option>
+          <option value="10">Severe Storms</option>
+          <option value="17">Snow</option>
+          <option value="12">Volcanos</option>
+          <option value="13">Water Color</option>
+          <option value="8">Wildfires</option>
+        </select>
+      </div>
     </div>
   );
 }
