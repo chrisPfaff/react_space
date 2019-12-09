e2deb9406c7448739771206412f79849be87e82a
Chris Pfaff <chrispfaff10@gmail.com>
2019-12-08 21:20:47 -0600

    heroku

---
+++ build/precache-manifest.568892b9463e1e8bfe180c23f5dbe272.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0310eae0d57001f38d6beb893e661cf6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8369725d1baae5e874fb",
+    "url": "/static/css/main.be0b4163.chunk.css"
+  },
+  {
+    "revision": "2b847d8c15933f6ff09e",
+    "url": "/static/js/2.2df1f72e.chunk.js"
+  },
+  {
+    "revision": "8369725d1baae5e874fb",
+    "url": "/static/js/main.e31fc736.chunk.js"
+  },
+  {
+    "revision": "cbee8d858c084296ccff",
+    "url": "/static/js/runtime-main.985e6003.js"
+  }
+]);
