8224431461f0a1fb5baa67458906ac51cd23e29d
Chris Pfaff <chrispfaff10@gmail.com>
2020-01-14 21:37:52 -0600

    fixing darkmode

--- client/src/components/Darkmode.js
+++ client/src/components/Darkmode.js
@@ -7,26 +7,35 @@ const Darkmode = props => {
   const clickFunction = e => {
     const body = document.querySelector("body");
     const links = document.querySelectorAll("a");
+    const para = document.querySelectorAll("p");
     const darkModeText = document.getElementsByClassName(
       "DarkmodeContainer_name"
     );
     const header = document.getElementsByClassName("App_header");
     const logo = document.getElementsByClassName("App_logo_dark")[0];
     const description = document.querySelector("h2");
+    const title = document.querySelector("h1");
 
     if (dark === false) {
       setDark(true);
       links.forEach(item => {
         item.classList.add("dark");
       });
+      para.forEach(item => {
+        item.classList.add("dark");
+      });
       body.classList.add("dark");
       darkModeText[0].classList.add("dark");
       header[0].classList.add("dark");
       description.classList.add("dark");
+      title.classList.add("dark");
       logo.style.zIndex = 2;
     } else {
       setDark(false);
       links.forEach(item => {
+        item.classList.remove("dark");
+      });
+      para.forEach(item => {
         item.classList.remove("dark");
       });
       body.classList.remove("dark");
